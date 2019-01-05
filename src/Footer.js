031c2e83d33cb1c9992909faccd7f3548549156d
José Higino <6281545+4Ykw@users.noreply.github.com>
2019-01-05 15:04:42 +1300

    Fix end of the line js code
    Java Script is interpreting end of the line and not adding space. Fixed by cutting the end of the line.

--- src/Footer.js
+++ src/Footer.js
@@ -5,8 +5,7 @@ const Footer = () => {
     <div className="jumbotron text-center bottom">
       <p>Made with ❤ by <a href="https://www.eosoceania.io">EOS Oceania</a></p>
       <p>Contribute on <a href="https://github.com/eosoceania/eosplorer">Github</a> or message us on <a href="https://t.me/joinchat/IB6xJg7tmo7v4knEJyQRSw">Telegram</a>.</p>
-      <p>Connect with us: <a href="https://t.me/joinchat/IB6xJg7tmo7v4knEJyQRSw">Telegram</a>
-        | <a href="https://discord.gg/eAdBZBv">Discord</a> | <a href="https://steemit.com/@eosoceania">Steemit</a>.</p>
+      <p>Connect with us: <a href="https://t.me/joinchat/IB6xJg7tmo7v4knEJyQRSw">Telegram</a> | <a href="https://discord.gg/eAdBZBv">Discord</a> | <a href="https://steemit.com/@eosoceania">Steemit</a>.</p>
     </div>
   );
 }
